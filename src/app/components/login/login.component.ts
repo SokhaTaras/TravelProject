3dcc3a9cbad44516c073f76edd5cd074dc4c9377
Taras <sokha2411@gmail.com>
2023-04-03 16:26:31 +0300

    small inhancements

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {OnInit} from "@angular/core";
+import {FormControl, FormGroup} from "@angular/forms";
+import {User} from "../../Classes/user";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  loginForm: FormGroup = new FormGroup({
+    'email': new FormControl(),
+    'password': new FormControl()
+  });
+  isRegistration: boolean = false;
+  newUser: User = new User("","");
+
+  ngOnInit() {}
+  constructor() {}
+
+
+  login(){
+    this.newUser.password = this.loginForm.get('password')?.value
+    this.newUser.email = this.loginForm.get('email')?.value
+    console.log(this.newUser)
+  }
 }
