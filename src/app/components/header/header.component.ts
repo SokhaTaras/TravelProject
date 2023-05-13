118bf3be9a1a3808d12f509ec1163b4797cab222
kiba659 <bohdanolesko@gmail.com>
2023-05-13 09:30:28 +0300

    new-style-main-regexp

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,15 @@
-import { Component, HostListener } from '@angular/core';
+ import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  isHeaderTransparent = true;
-
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll() {
-    this.isHeaderTransparent = window.scrollY < 40;
-  }
+  export class HeaderComponent {
+  
+// це відповідає за появу хедера під час скролу
+//   @HostListener('window:scroll', ['$event'])
+//   onWindowScroll() {
+//     this.isHeaderTransparent = window.scrollY < 40;
+//   }
 }
