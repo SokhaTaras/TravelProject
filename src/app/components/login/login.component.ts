b4b2a59715f2716d0fc303ca4b1ad8d8404cc9b9
Taras <sokha2411@gmail.com>
2023-05-24 19:12:54 +0300

    endpoints are ready

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,8 @@ import {OnInit} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import { ActivatedRoute } from '@angular/router';
 import {User} from "../../Classes/user";
+import {LoginService} from "../../services/login-service/login.service";
+import {newUser} from "../../Classes/newUser";
 
 @Component({
   selector: 'app-login',
@@ -17,12 +19,13 @@ export class LoginComponent implements OnInit{
   });
 
   registrationForm: FormGroup = new FormGroup({
-    'userName': new FormControl('',[Validators.required]),
-    'userSurname': new FormControl('',[Validators.required]),
+    'firstName': new FormControl('',[Validators.required]),
+    'lastName': new FormControl('',[Validators.required]),
     'email': new FormControl('',[Validators.required, Validators.email]),
     'password': new FormControl('',[Validators.required]),
   })
-  newUser: User = new User("","");
+  logUser: User = new User('','');
+  newUser: newUser = new newUser('','','','');
   isLogging: boolean = true
   route: any
 
@@ -31,24 +34,30 @@ export class LoginComponent implements OnInit{
     this.switchIsLogging()
   }
 
-  constructor(private activateRoute: ActivatedRoute){
-  }
+  constructor(private activateRoute: ActivatedRoute, private loginService: LoginService){}
 
-  loggingToggler(){
+  loggingToggler(): void{
     this.isLogging = !this.isLogging;
   }
 
-  login(){
-    this.newUser.password = this.loginForm.get('password')?.value
-    this.newUser.email = this.loginForm.get('email')?.value
-    console.log(this.newUser)
+  login(): void {
+    const { password, email } = this.loginForm.value;
+    const logUser = { password, email };
+    console.log('logUser:', logUser);
+    this.loginService.loginUser(logUser).subscribe(res => console.log(res));
+    alert('you are logged in');
   }
 
-  register(){
-
+  register(): void {
+    const { firstName, lastName, email, password } = this.registrationForm.value;
+    const newUser = { firstName, lastName, email, password };
+    console.log('newUser:', newUser);
+    this.loginService.registerUser(newUser).subscribe(res => console.log(res));
+    alert('Confirmation email was sent')
   }
 
-  switchIsLogging(){
+
+  switchIsLogging(): void{
     this.route = this.activateRoute.url.subscribe(value => {
       if (value[0].path === 'registration'){
         this.isLogging = false
--- src/app/components/tours-page/tours-page.component.ts
+++ src/app/components/tours-page/tours-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {TripService} from "../../services/trip-service/trip.service";
 import {Cities, Countries} from "../../Enums/enums";
 
@@ -19,7 +19,7 @@ export class ToursPageComponent implements OnInit{
     selectedCountry: string = '';
     selectedCity: string = '';
 
-    constructor(private tripService: TripService) {}
+    constructor(private tripService: TripService, private cdr: ChangeDetectorRef) {}
     ngOnInit(): void {
       this.getAllTrips()
     }
@@ -37,6 +37,7 @@ export class ToursPageComponent implements OnInit{
       });
     } else {
       this.getAllTrips();
+
     }
   }
 
@@ -56,6 +57,53 @@ export class ToursPageComponent implements OnInit{
     this.tripService.getTripsByCity(this.selectedCity).subscribe((res) => {
       this.tours = res;
     });
+  }
+
+
+  // filterTrips(): void {
+  //   let filteredTours = this.tours;
+  //   console.log('filter',filteredTours);
+  //   // Фільтрація за тривалістю
+  //   if (this.duration > 0) {
+  //     filteredTours = filteredTours.filter((tour: any) => {
+  //       console.log(tour.duration === this.duration)
+  //       return tour.duration === this.duration;
+  //     });
+  //     console.log('dur',filteredTours);
+  //   }
+  //
+  //   // Фільтрація за ціною
+  //   if (this.minPrice > 0 && this.maxPrice > 0) {
+  //     filteredTours = filteredTours.filter((tour: any) => tour.price >= this.minPrice && tour.price <= this.maxPrice);
+  //     console.log('price',filteredTours);
+  //   }
+  //
+  //   // Фільтрація за країною
+  //   if (this.selectedCountry) {
+  //     filteredTours = filteredTours.filter((tour: any) => tour.countries.split(',').includes(this.selectedCountry));
+  //   }
+  //
+  //   // Фільтрація за містом
+  //   if (this.selectedCity) {
+  //     filteredTours = filteredTours.filter((tour: any) => tour.city === this.selectedCity);
+  //     console.log('city',filteredTours);
+  //   }
+  //   console.log('filter2',filteredTours);
+  //   this.tours = filteredTours;
+  // }
+
+  resetFilters(): void {
+    this.duration = 0;
+    this.minPrice = 0;
+    this.maxPrice = 0;
+    this.selectedCountry = '';
+    this.selectedCity = '';
+
+    this.getAllTrips();
+  }
+
+  ngAfterViewChecked(): void {
+    this.cdr.detectChanges();
   }
 
 }
